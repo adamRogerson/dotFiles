1434270615bfd085a63c57539ed5c8c1a7c4b89a
arogerson <arogerson@druidsoftware.com>
2019-04-11 13:05:35 +0100

    removed old ref to previous config and adding current

---
+++ fish
@@ -0,0 +1,1 @@
+/home/arogerson/.config/fish/
--- fish_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt
-
-    set base (basename (pwd))
-    set date (date "+%I:%M")
-    echo [(set_color yellow)$base (set_color white)$date]'> '
-    
-end
