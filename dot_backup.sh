e09cfbdcf145c02d16c72cb69ae9473b3a7125cf
arogerson <arogerson@druidsoftware.com>
2019-04-16 14:40:16 +0100

    Amended auto backup file to ignore history file in fish

--- dot_backup.sh
+++ dot_backup.sh
@@ -2,7 +2,8 @@
 #Dumb script to be called by cron to backup dot files and push to git if fail will post mail
 
 cp -r ~/.vim/ ~/Software/dotFiles/.vim
-cp -r ~/.config/fish ~/Software/dotFiles/fish
+cp -r ~/.config/config.fish ~/Software/dotFiles/fish
+cp -r ~/.config/fish/functions ~/Software/dotFiles/fish
 
 pushd ~/Software/dotFiles/
 git add .
