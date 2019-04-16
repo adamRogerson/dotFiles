a1a1957f5f8e091cb87a6c57729a21d069469af4
arogerson <arogerson@druidsoftware.com>
2019-04-16 14:17:19 +0100

    more additons to script

--- dot_backup.sh
+++ dot_backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Dumb script to be called by cron to backup dot files and push to git if fail will post mail
+
 cp -r ~/.vim/ ~/Software/dotFiles/.vim
 cp -r ~/.config/fish ~/Software/dotFiles/fish
 
