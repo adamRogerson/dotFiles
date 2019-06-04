36058fadae536c72f1e561699b826a19c3f1301e
adamRogerson <adam.thomas.rogerson@gmail.com>
2019-06-04 11:58:58 +0100

    Auto backup via cron

--- dot_backup.sh
+++ dot_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Dumb script to be called by cron to backup dot files and push to git if fail will post mail
 
-cp -r ~/.vim/ ~/Software/dotFiles/.vim
-cp -r ~/.config/config.fish ~/Software/dotFiles/fish/
-cp -r ~/.config/fish/functions ~/Software/dotFiles/fish/function
-
 pushd ~/Software/dotFiles/
+
+rsync -a --exclude '.git' ~/.vim .
+rsync -a  ~/.config/fish/functions ./fish
+
 git add .
 git commit -m "Auto backup via cron"
 git push
-
+popd
